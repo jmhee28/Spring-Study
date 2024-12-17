0edf763beb1404e3aa4b6c2dc3cfd49b5db182f0
Joung MyeongHee <jmhee3411@gmail.com>
2024-12-17 17:17:24 +0900

    섹션4.새로운 할인 정책 개발

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -7,6 +7,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 class RateDiscountPolicyTest {
     RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
     @Test
@@ -17,6 +19,20 @@ class RateDiscountPolicyTest {
         //when
         int discount = discountPolicy.discount(member, 10000);
         //then
-        Assertions.assertThat(discount).isEqualTo(1000);
+        assertThat(discount).isEqualTo(1000);
     }
+
+    // 실패 테스트
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
+    void vip_x(){
+        //given
+        Member member= new Member(2L, "memberBASIC", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        // static import(opt + enter)를 사용하면 assertThat을 사용할 때 클래스명을 생략할 수 있다.
+        assertThat(discount).isEqualTo(0);
+    }
+
 }
