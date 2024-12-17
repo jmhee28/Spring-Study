3c8faddccb6ab07f8d868cd9b580e33e815b34de
Joung MyeongHee <jmhee3411@gmail.com>
2024-12-17 17:11:12 +0900

    섹션3.주문 할인 도메인 실행과 테스트

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,16 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+    private int discountPercent = 10;
+    @Override
+    public int discount(hello.core.member.Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
+
---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        // discountPolicy에서 할인 금액을 받아온다.
+        // 단일 책임원칙을 잘 지킨것
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
