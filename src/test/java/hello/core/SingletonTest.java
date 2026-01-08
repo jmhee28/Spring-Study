8d65c628baeb719a705a1aaa6d2135649e3b618c
정명희 <mhee@nhn.com>
2026-01-08 17:12:44 +0900

    test: apply singleton

--- src/test/java/hello/core/SingletonTest.java
+++ src/test/java/hello/core/SingletonTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 
 import hello.core.member.MemberService;
 
+
 public class SingletonTest {
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
-
     void pureContainer(){
         AppConfig appConfig = new AppConfig();
         MemberService memberService1 = appConfig.memberService();     
@@ -20,5 +20,23 @@ public class SingletonTest {
 
 
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+
+    // 호출할 때 마다 같은 객체 인스턴스를 반환하는 것을 확인
+    // 참고: 싱글톤 패턴을 구현하는 방법은 여러 가지가 있으나 여기서는 객체를 미리 생성해두는 가장 단순하고 안전한 방법을 선택
+
+    // 싱글톤 패턴을 적용하면 고객의 요청이 올 때 마다 객체를 생성하는 것이 아니라, 이미 만들어진 객체를 공유해서 효율적으로 사용할 수 있음.
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+
     }
 }
